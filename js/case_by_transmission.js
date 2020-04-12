0757ba4577d8caa96962eddb329bbdd073e62dc6
Linc <calin@roe.ac.uk>
2020-04-12 20:44:49 +0800

    Animation on bars

--- js/case_by_transmission.js
+++ js/case_by_transmission.js
@@ -1,26 +1,121 @@
 
 
-//-- Dimension & margin
-var totWidth = 800;
-var totHeight = 400;
-var margin = {left: 70, right: 0, bottom: 90, top: 0};
-var width = totWidth - margin.left - margin.right;
-var height = totHeight - margin.top - margin.bottom;
-var corner = [[0, 0], [width, 0], [0, height], [width, height]];
-
 //-- Convert date format
 function ISODateToMDDate(ISODate) {
   var MDDateFormat = d3.timeFormat("%b %d");
   return MDDateFormat(d3.isoParse(ISODate));
 }
 
-//-- Calculations
-function dailySum(row) {
-  return +row["indigenous unlinked"] + +row["indigenous linked"] + +row["imported"];
+function cumsum(data, colTagList) {
+  var i, j;
+  for (i=1; i<data.length; i++) {
+    for (j=0; j<colTagList.length; j++) {
+      data[i][colTagList[j]] = +data[i][colTagList[j]] + +data[i-1][colTagList[j]];
+    }
+  }
+}
+
+var DC_wrap = {};
+DC_wrap.id = "#case_by_transmission"
+DC_wrap.dataPath = "processed_data/Taiwan_simplified.csv";
+
+function DC_makeCanvas() {
+  var totWidth = 800;
+  var totHeight = 400;
+  var margin = {left: 70, right: 0, bottom: 90, top: 0};
+  var width = totWidth - margin.left - margin.right;
+  var height = totHeight - margin.top - margin.bottom;
+  var corner = [[0, 0], [width, 0], [0, height], [width, height]];
+  
+  var svg = d3.select(DC_wrap.id)
+    .append("svg")
+      .attr("viewBox", "0 0 " + totWidth + " " + totHeight)
+      .attr("preserveAspectRatio", "xMinYMin meet")
+    .append("g")
+      .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+  
+  DC_wrap.totWidth = totWidth;
+  DC_wrap.totHeight = totHeight;
+  DC_wrap.margin = margin;
+  DC_wrap.width = width;
+  DC_wrap.height = height;
+  DC_wrap.corner = corner;
+  DC_wrap.svg = svg;
+}
+
+function DC_formatData(data, doCumul) {
+  //-- Settings for xticklabels
+  var xlabel_path = 7;
+  var q = data.length % xlabel_path;
+  var rList = [3, 3, 4, 1, 1, 2, 2];
+  var r = rList[q];
+  var xtick = [];
+  var xticklabel = [];
+  var ymax = 0;
+  
+  var colTagList = data.columns.slice(1);
+  var dateList = [];
+  var formattedData = [];
+  var i, j, x, y, height, block;
+
+  if (doCumul == 1) {
+    cumsum(data, colTagList);
+  }
+  
+  for (i=0; i<data.length; i++) {
+    y = 0;
+    x = data[i]["date"];
+    dateList.push(x);
+    
+    for (j=0; j<colTagList.length; j++) {
+      height = +data[i][colTagList[j]];
+      block = {
+        'x': x,
+        'y0': y,
+        'y1': y + height,
+        'height': height,
+        'h1': +data[i][colTagList[2]],
+        'h2': +data[i][colTagList[1]],
+        'h3': +data[i][colTagList[0]],
+        'col': colTagList[j]
+      };
+        
+      y += height;
+      formattedData.push(block);
+    }
+    
+    ymax = Math.max(ymax, y);
+    
+    if (i % xlabel_path == r) {
+      xtick.push(i+0.5)
+      xticklabel.push(ISODateToMDDate(x));
+    }
+    else {
+      xticklabel.push("");
+    }
+  }
+  
+  //-- Calculate ymax
+  ymax *= 1.11;
+  var ypath;
+  if (doCumul == 1) ypath = 100; //Math.floor(ymax / 5);
+  else              ypath = 5;
+  
+  var ytick = [];
+  for (i=0; i<ymax; i+=ypath) ytick.push(i)
+  
+  DC_wrap.formattedData = formattedData;
+  DC_wrap.dateList = dateList;
+  DC_wrap.colTagList = colTagList;
+  DC_wrap.ymax = ymax;
+  DC_wrap.xtick = xtick;
+  DC_wrap.xticklabel = xticklabel;
+  DC_wrap.ytick = ytick;
 }
 
 //-- Tooltip
-var tooltip = d3.select("#case_by_transmission")
+var tooltip = d3.select(DC_wrap.id)
   .append("div")
   .attr("class", "tooltip")
 
@@ -39,7 +134,7 @@ function getTooltipPos(d) {
   
   //-- Look for the furthest vertex
   for (i=0; i<4; i++) {
-    l = (d[0] - corner[i][0])**2 + (d[1] - corner[i][1])**2;
+    l = (d[0] - DC_wrap.corner[i][0])**2 + (d[1] - DC_wrap.corner[i][1])**2;
     if (l > l_max) {
       l_max = l;
       i_max = i;
@@ -48,20 +143,20 @@ function getTooltipPos(d) {
   
   //-- Place the caption somewhere on the longest arm, parametrizaed by xAlpha & yAlpha
   var xAlpha = 0.1;
-//   var yAlpha = 0.5;
-  var yAlpha = 1;
-  var xPos = d[0] * (1-xAlpha) + corner[i_max][0] * xAlpha;
-  var yPos = d[1] * (1-yAlpha) + corner[i_max][1] * yAlpha;
+  var yAlpha = 0.5;
+//   var yAlpha = 1;
+  var xPos = d[0] * (1-xAlpha) + DC_wrap.corner[i_max][0] * xAlpha;
+  var yPos = d[1] * (1-yAlpha) + DC_wrap.corner[i_max][1] * yAlpha;
   
   var buffer = 1.25*16; //-- Margin buffer of card-body
   var button = (0.9+0.875)*16 + 20; //-- Offset caused by button
   var cardHdr = 3.125*16; //-- Offset caused by card-header
-  var svgDim = d3.select("#case_by_transmission").node().getBoundingClientRect();
-  var xAspect = (svgDim.width - 2*buffer) / totWidth;
-  var yAspect = (svgDim.height - 2*buffer) / totHeight;
+  var svgDim = d3.select(DC_wrap.id).node().getBoundingClientRect();
+  var xAspect = (svgDim.width - 2*buffer) / DC_wrap.totWidth;
+  var yAspect = (svgDim.height - 2*buffer) / DC_wrap.totHeight;
   
-  xPos = (xPos + margin.left) * xAspect + buffer;
-  yPos = (yPos + margin.top) * yAspect + buffer + cardHdr + button;
+  xPos = (xPos + DC_wrap.margin.left) * xAspect + buffer;
+  yPos = (yPos + DC_wrap.margin.top) * yAspect + buffer + cardHdr + button;
   
   return [xPos, yPos];
 }
@@ -69,7 +164,7 @@ function getTooltipPos(d) {
 function mousemove(d) {
   var newPos = getTooltipPos(d3.mouse(this));
   tooltip
-    .html(d.data["date"] + "<br>Total = " + dailySum(d.data) + "<br>Imported = " + d.data["imported"] + "<br>Ind. linked = " + d.data["indigenous linked"] + "<br>Ind. unlinked = " + d.data["indigenous unlinked"])
+    .html(d.x + "<br>Total = " + (+d.h1 + +d.h2 + +d.h3) + "<br>Imported = " + d.h1+ "<br>Ind. linked = " + d.h2 + "<br>Ind. unlinked = " + d.h3)
     .style("left", newPos[0] + "px")
     .style("top", newPos[1] + "px")
 }
@@ -82,155 +177,100 @@ function mouseleave(d) {
     .style("opacity", 1)
 }
 
-function cumsum(data) {
-  var i;
-  
-  for (i=1; i<data.length; i++) {
-    data[i]["imported"]          = +data[i]["imported"] + +data[i-1]["imported"];
-    data[i]["indigenous linked"] = +data[i]["indigenous linked"] + +data[i-1]["indigenous linked"];
-    data[i]["indigenous unlinked"] = +data[i]["indigenous unlinked"] + +data[i-1]["indigenous unlinked"];
-  }
-}
-
-//-- Append svg object to the body of the page
-var svg = d3.select("#case_by_transmission")
-  .append("svg")
-    .attr("viewBox", "0 0 " + totWidth + " " + totHeight)
-    .attr("preserveAspectRatio", "xMinYMin meet")
-  .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
-
-function plotBase(error, data, doCumul) {
-  if (error) throw error;
-  
-  if (doCumul == 1) {
-    cumsum(data);
-  }
-  
-//   console.log(data)
-  
-  
-  //-- Settings for xticklabels
-  var xlabel_path = 7;
-  var q = data.length % xlabel_path;
-  var rList = [3, 3, 4, 1, 1, 2, 2];
-  var r = rList[q];
-  
-  var xtick = [];
-  var xticklabel = [];
-  var ymax = 0;
-  var i;
-  
-  //-- Get xticks, xticklabels, & ymax
-  for (i=0; i<data.length; i++) {
-    ymax = Math.max(ymax, dailySum(data[i]));
-    
-    if (i % xlabel_path == r) {
-      xtick.push(i+0.5)
-      xticklabel.push(ISODateToMDDate(data[i]["date"]));
-    }
-    else {
-      xticklabel.push("");
-    }
-  }
-  
-  //-- Calculate ymax
-  ymax *= 1.25;
-  var ypath;
-  if (doCumul == 1) ypath = 100; //Math.floor(ymax / 5);
-  else              ypath = 5;
-  var ytick = [];
-  for (i=0; i<ymax; i+=ypath) ytick.push(i)
-  
-  //-- Rows & columns
-  var colList = data.columns.slice(1);
-  var dateList = d3.map(data, function(d){return(d["date"])}).keys();
-
+function DC_initialize() {
   //-- Add x-axis
   var x = d3.scaleBand()
-    .domain(dateList)
-    .range([0, width])
-    .padding([0.2])
+    .range([0, DC_wrap.width])
+    .domain(DC_wrap.dateList)
+    .padding(0.2);
+    
   var xAxis = d3.axisBottom(x)
     .tickSize(0)
-    .tickFormat(function(d, i){return xticklabel[i]});
-  svg.append("g")
-    .attr("transform", "translate(0," + height + ")")
-    .attr("class", "plot")
+    .tickFormat(function(d, i){return DC_wrap.xticklabel[i]});
+  
+  DC_wrap.svg.append('g')
+    .attr('class', 'xaxis')
+    .attr('transform', 'translate(0,' + DC_wrap.height + ')')
     .call(xAxis)
     .selectAll("text")
       .attr("transform", "translate(-8,15) rotate(-90)")
       .style("text-anchor", "end")
-  
+    
   //-- Add a 2nd x-axis for ticks
   var x2 = d3.scaleLinear()
-    .domain([0, data.length])
-    .range([0, width])
+    .domain([0, DC_wrap.dateList.length])
+    .range([0, DC_wrap.width])
+  
   var xAxis2 = d3.axisBottom(x2)
-    .tickValues(xtick)
+    .tickValues(DC_wrap.xtick)
     .tickSize(10)
     .tickSizeOuter(0)
     .tickFormat(function(d, i){return ""});
-  svg.append("g")
-    .attr("transform", "translate(0," + height + ")")
-    .attr("class", "plot")
+  
+  DC_wrap.svg.append("g")
+    .attr("transform", "translate(0," + DC_wrap.height + ")")
+    .attr("class", "xaxis")
     .call(xAxis2)
   
   //-- Add y-axis
   var y = d3.scaleLinear()
-    .domain([0, ymax])
-    .range([height, 0]);
+    .domain([0, DC_wrap.ymax])
+    .range([DC_wrap.height, 0]);
+  
   var yAxis = d3.axisLeft(y)
-    .tickSize(-width)
-    .tickValues(ytick)
-  svg.append("g")
-    .attr("class", "plot")
+    .tickSize(-DC_wrap.width)
+    .tickValues(DC_wrap.ytick)
+  
+  DC_wrap.svg.append("g")
+    .attr("class", "yaxis")
     .call(yAxis)
 
   //-- Add a 2nd y-axis for the frameline at right
   var yAxis2 = d3.axisRight(y)
     .ticks(0)
     .tickSize(0)
-  svg.append("g")
-    .attr("class", "plot")
-    .attr("transform", "translate("+width+",0)")
+  
+  DC_wrap.svg.append("g")
+    .attr("class", "yaxis")
+    .attr("transform", "translate(" + DC_wrap.width + ",0)")
     .call(yAxis2)
-
-  //-- Colors
-  var color = d3.scaleOrdinal()
-    .domain([colList[2], colList[1], colList[0]])
-    .range(['#3366BB', '#CC6677', '#55BB44'])
-
+    
   //-- ylabel
-  svg.append("text")
-    .attr("class", "plot")
+  DC_wrap.svg.append("text")
+    .attr("class", "ylabel")
     .attr("text-anchor", "middle")
-    .attr("transform", "translate(" + (-margin.left*0.75).toString() + ", " + (height/2).toString() + ")rotate(-90)")
+    .attr("transform", "translate(" + (-DC_wrap.margin.left*0.75).toString() + ", " + (DC_wrap.height/2).toString() + ")rotate(-90)")
     .text("Number counts");
     
+  //-- Color
+  colorList = ['#3366BB', '#CC6677', '#55BB44', '#000000']
+  var color = d3.scaleOrdinal()
+    .domain([DC_wrap.colTagList[2], DC_wrap.colTagList[1], DC_wrap.colTagList[0], 'none'])
+    .range(colorList)
+
   //-- Legend
   var x_legend = 90;
-  var y_legend = 45;
+  var y_legend = 40;
   
   //-- Legend - circles
   label = ["Imported", "Indigenous linked to known cases", "Indigenous unlinked"]
-  svg.selectAll("dot")
+  DC_wrap.svg.selectAll("dot")
     .data(label)
     .enter()
     .append("circle")
-      .attr("class", "plot")
+      .attr("class", "legend")
       .attr("cx", x_legend)
       .attr("cy", function(d,i) {return y_legend + i*25})
       .attr("r", 7)
       .style("fill", function(d) {return color(d)})
       
   //-- Legend - texts
-  svg.selectAll("label")
+  label.push("Total")
+  DC_wrap.svg.selectAll("label")
     .data(label)
     .enter()
     .append("text")
-      .attr("class", "plot")
+      .attr("class", "legend")
       .attr("x", x_legend+20)
       .attr("y", function(d,i){ return y_legend + i*25 + 7})
       .style("fill", function(d){ return color(d)})
@@ -239,23 +279,15 @@ function plotBase(error, data, doCumul) {
       .style("alignment-baseline", "middle")
   
   //-- Legend - total cases
-  var Im_sum, IL_sum, IU_sum;
-  if (doCumul == 1) {
-    Im_sum = data[data.length-1]["imported"];
-    IL_sum = data[data.length-1]["indigenous linked"];
-    IU_sum = data[data.length-1]["indigenous unlinked"];
-  }
-  else {
-    Im_sum = d3.sum(data, function(d){return +d["imported"];});
-    IL_sum = d3.sum(data, function(d){return +d["indigenous linked"];});
-    IU_sum = d3.sum(data, function(d){return +d["indigenous unlinked"];});
-  }
+  var Im_sum = d3.sum(DC_wrap.formattedData, function(d){if (d.col == 'imported') return +d.h1;});
+  var IL_sum = d3.sum(DC_wrap.formattedData, function(d){if (d.col == 'indigenous linked') return +d.h2;});
+  var IU_sum = d3.sum(DC_wrap.formattedData, function(d){if (d.col == 'indigenous unlinked') return +d.h3;});
   
-  svg.selectAll("sum")
-    .data([Im_sum, IL_sum, IU_sum])
+  DC_wrap.svg.selectAll("sum")
+    .data([Im_sum, IL_sum, IU_sum, Im_sum+IL_sum+IU_sum])
     .enter()
     .append("text")
-      .attr("class", "plot")
+      .attr("class", "legend")
       .attr("x", x_legend-20)
       .attr("y", function(d,i){ return y_legend + i*25 + 7})
       .style("fill", function(d){ return color(d)})
@@ -263,49 +295,72 @@ function plotBase(error, data, doCumul) {
       .attr("text-anchor", "end")
       .style("alignment-baseline", "middle")
   
-  //-- Stack data
-  var stackedData = d3.stack()
-    .keys(colList)(data)
-  
-  //-- Plot
-  svg.append("g")
-    .selectAll("g")
-    .data(stackedData)
-    .enter().append("g")
-      .attr("fill", function(d) {return color(d.key);})
-      .selectAll("rect")
-      .data(function(d) {return d;})
-      .enter().append("rect")
-        .attr("class", "plot")
-        .attr("x", function(d) {return x(d.data["date"]);})
-        .attr("y", function(d) {return y(d[1]);})
-        .attr("height", function(d) {return y(d[0]) - y(d[1]);})
-        .attr("width", x.bandwidth())
+  //-- Bar
+  var bar = DC_wrap.svg.selectAll('.bar')
+    .data(DC_wrap.formattedData)
+    .enter();
+        
+  bar.append('rect')
+    .attr('class', 'bar')
+    .attr('fill', function(d){ return color(d.col);})
+    .attr('x', function(d) { return x(d.x);})
+    .attr('y', function(d) {return y(0);})
+    .attr('width', x.bandwidth())
+    .attr('height', 0)
     .on("mouseover", mouseover)
     .on("mousemove", mousemove)
     .on("mouseleave", mouseleave)
+
+  DC_wrap.bar = bar;
 }
 
-function plotDaily(error, data) {
-  plotBase(error, data, 0)
+function DC_update() {
+  var transDuration = 800;
+
+  //-- Add y-axis
+  var y = d3.scaleLinear()
+    .domain([0, DC_wrap.ymax])
+    .range([DC_wrap.height, 0]);
+  
+  var yAxis = d3.axisLeft(y)
+    .tickSize(-DC_wrap.width)
+    .tickValues(DC_wrap.ytick)
+  
+  DC_wrap.svg.select('.yaxis')
+    .transition()
+    .duration(transDuration)
+    .call(yAxis);
+  
+  //-- Update bars
+  DC_wrap.bar.selectAll("rect")
+    .data(DC_wrap.formattedData)
+    .transition()
+    .duration(transDuration)
+    .attr('y', function(d) {return y(d.y1);})
+    .attr('height', function(d) {return y(d.y0)-y(d.y1);});
 }
 
-function plotCumul(error, data) {
-  plotBase(error, data, 1)
-}
-
-//-- Parse data
-d3.csv("processed_data/Taiwan_simplified.csv", plotDaily)
+d3.csv(DC_wrap.dataPath, function(error, data){
+  DC_makeCanvas();
+  DC_formatData(data, 0);
+  DC_initialize();
+  DC_update();
+})
 
 //-- Button listener
-$(document).on("change", "input:radio[name='doCumul']", function (event) {
+$(document).on("change", "input:radio[name='case_by_transmission_doCumul']", function(event) {
   if (this.value == "daily") {
-    d3.select("#case_by_transmission").selectAll(".plot").remove();
-    d3.csv("processed_data/Taiwan_simplified.csv", plotDaily);
+    doCumul = 0;
   }
   else {
-    d3.select("#case_by_transmission").selectAll(".plot").remove();
-    d3.csv("processed_data/Taiwan_simplified.csv", plotCumul);
+    doCumul = 1;
   }
+  
+  d3.csv(DC_wrap.dataPath, function(error, data){
+    DC_formatData(data, doCumul);
+    DC_update();
+  });
 });
+
+  
 
