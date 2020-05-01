e8101d864159e9b9f0adf936f9a9f112a41d83be
Linc <calin@roe.ac.uk>
2020-05-01 19:17:47 +0800

    New page in upload script

--- upload.sh
+++ upload.sh
@@ -16,15 +16,18 @@ echo
 echo "## Download raw data"
 echo
 wget -O 'raw_data/武漢肺炎in臺灣相關整理(請看推廣用連結) - 臺灣武漢肺炎病例.csv' 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRM7gTCUvuCqR3zdcLGccuGLv1s7dpDcQ-MeH_AZxnCXtW4iqVmEzUnDSKR7o8OiMLPMelEpxE7Pi4Q/pub?output=csv&gid=0' &
-sleep 10
+sleep 7
 echo
 wget -O 'raw_data/武漢肺炎in臺灣相關整理(請看推廣用連結) - 檢驗人數.csv' 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRM7gTCUvuCqR3zdcLGccuGLv1s7dpDcQ-MeH_AZxnCXtW4iqVmEzUnDSKR7o8OiMLPMelEpxE7Pi4Q/pub?output=csv&gid=1173642744' &
-sleep 10
+sleep 7
+echo
+wget -O 'raw_data/武漢肺炎in臺灣相關整理(請看推廣用連結) - 時間軸.csv' 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRM7gTCUvuCqR3zdcLGccuGLv1s7dpDcQ-MeH_AZxnCXtW4iqVmEzUnDSKR7o8OiMLPMelEpxE7Pi4Q/pub?output=csv&gid=1744708886' &
+sleep 7
 echo
 
 echo "## Process data"
 echo
-/usr/bin/python3.8 COVID_breakdown_data_processing.py & sleep 5
+/usr/bin/python3.8 COVID_breakdown_data_processing.py & sleep 3
 echo
 
 echo "## Push to repo"
@@ -32,7 +35,7 @@ echo
 git add processed_data/*.csv & sleep 1
 git commit -m "${MODE} data update" & sleep 1
 echo
-git push & sleep 10
+git push & sleep 7
 echo
 
 date
