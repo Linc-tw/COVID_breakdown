a8952b20ec89e10984d659a252bf9728216e733f
Linc-tw <turtlelin1210@gmail.com>
2020-12-03 21:50:58 +0800

    Updated upload.sh

--- upload.sh
+++ upload.sh
@@ -16,33 +16,34 @@ echo
 echo "## Download raw data"
 echo
 wget -O 'raw_data/COVID-19_in_Taiwan_raw_data_case_breakdown.csv' 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRM7gTCUvuCqR3zdcLGccuGLv1s7dpDcQ-MeH_AZxnCXtW4iqVmEzUnDSKR7o8OiMLPMelEpxE7Pi4Q/pub?output=csv&gid=0' &
-sleep 7
+sleep 5
 echo
 wget -O 'raw_data/COVID-19_in_Taiwan_raw_data_status_evolution.csv' 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRM7gTCUvuCqR3zdcLGccuGLv1s7dpDcQ-MeH_AZxnCXtW4iqVmEzUnDSKR7o8OiMLPMelEpxE7Pi4Q/pub?output=csv&gid=1036676170' &
-sleep 7
+sleep 5
 echo
 wget -O 'raw_data/COVID-19_in_Taiwan_raw_data_number_of_tests.csv' 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRM7gTCUvuCqR3zdcLGccuGLv1s7dpDcQ-MeH_AZxnCXtW4iqVmEzUnDSKR7o8OiMLPMelEpxE7Pi4Q/pub?output=csv&gid=1173642744' &
-sleep 7
+sleep 5
 echo
 wget -O 'raw_data/COVID-19_in_Taiwan_raw_data_border_statistics.csv' 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRM7gTCUvuCqR3zdcLGccuGLv1s7dpDcQ-MeH_AZxnCXtW4iqVmEzUnDSKR7o8OiMLPMelEpxE7Pi4Q/pub?output=csv&gid=1449990493' &
-sleep 7
+sleep 5
 echo
 wget -O 'raw_data/COVID-19_in_Taiwan_raw_data_timeline.csv' 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRM7gTCUvuCqR3zdcLGccuGLv1s7dpDcQ-MeH_AZxnCXtW4iqVmEzUnDSKR7o8OiMLPMelEpxE7Pi4Q/pub?output=csv&gid=1744708886' &
-sleep 12
+sleep 8
 echo
 
 echo "## Process data"
 echo
-/usr/bin/python3.8 COVID_breakdown_data_processing.py & sleep 3
+/usr/bin/python3.8 COVID_breakdown_data_processing.py & sleep 2
 echo
 
 echo "## Push to repo"
 echo
 git add raw_data/*.csv & sleep 1
-git add processed_data/*.csv & sleep 1
+git add processed_data/latest/*.csv & sleep 1
+git add processed_data/2020/*.csv & sleep 1
 git commit -m "${MODE} data update" & sleep 1
 echo
-git push & sleep 7
+git push & sleep 5
 echo
 
 date
