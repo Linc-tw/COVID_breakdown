fa7dd9e30c2aff1e79b349b67cf11d68095a60e6
Linc-tw <turtlelin1210@gmail.com>
2021-09-30 14:41:35 +0800

    Automatic data update

--- js/plot/plot_index.js
+++ js/plot/plot_index.js
@@ -6,7 +6,7 @@
 //--   Chieh-An Lin
 
 var PI_plot_list = [
-  [SE_Main, SE_latest_wrap, 1000],
+  [CC_Main, CC_latest_wrap, 1000],
   [VP_Main, VP_latest_wrap, 1000],
   [CC_Main, CC_mini_wrap, 0],
   [IR_Main, IR_mini_wrap, 0],
