f07ce5a69b5a2ee69e09847fb37e46aff49466d0
Linc-tw <turtlelin1210@gmail.com>
2022-04-22 16:46:33 +0800

    Automatic data update

--- js/utility/general_plotting.js
+++ js/utility/general_plotting.js
@@ -1226,7 +1226,7 @@ function GP_PlotHotMap(wrap) {
       .attr('text-anchor', 'middle')
       .attr('dominant-baseline', 'central')
       .style('fill', function (d) {if (d.value<0.5*wrap.value_max) return '#000000'; return '#FFFFFF';})
-      .style('font-size', '0.9rem')
+      .style('font-size', function (d) {if (d.value.toFixed(0)<100) return '0.9rem'; return '0.75rem';})
       .text(function (d) {if (d.value<0.5001) return ''; return d.value.toFixed(0);});
 }
 
